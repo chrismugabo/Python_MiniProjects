5386efc9e76b3abb46db43754de24d2c2b084601
cmugabo <c.mugabo@alustudent.com>
2024-07-05 15:07:02 -0400

    2

---
+++ Tips calc.py
@@ -0,0 +1,8 @@
+print("Welcome to the Tip Calculator")
+bill=int(input("What was the total bill? $"))
+tip=int(input("How much would you like tip?,10,12 or 15:150 "))
+n=int(input("how many people should split the bill?"))
+tbill=bill +(bill *(tip/100))
+
+total=tbill/5
+print(f"each person should pay {total,2}")
